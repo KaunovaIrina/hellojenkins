84f7a7a5ee989c4ce441b933203b342fa249e062
KaunovaIrina <irina987583@gmail.com>
2025-12-24 09:48:24 +0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ port = int(os.getenv('PORT', 5000))
 
 @app.route('/')
 def hello():
-    return f'<h1>Привет, меня зовут {student_name}!</h1><p>Порт: {port}</p>'
+    return f'<h1>Меня зовут {student_name}!</h1><p>Порт: {port}</p>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
